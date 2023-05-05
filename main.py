8e7570567feaf759b32ff52af1e16da2f76525a0
NiggetChuckens <rodrigoalonsoaltamirano@gmail.com>
2023-05-05 02:44:39 -0400

    New features added

--- main.py
+++ main.py
@@ -3,46 +3,80 @@ import sys
 import pysrt
 import openai
 
-openai.api_key = ''
+openai.api_key = '' #API key
+
 
 #Transcribe an audio
+def transcript_audio(audio_file):
+    audio_file=open(audio_file, 'rb') #audio file path
+    transcript=openai.Audio.transcribe(
+        file = audio_file,
+        model ="whisper-1", 
+        response_format='srt',  #select output file format (srt, text)
+        languaje="jp"   #Define languaje of the audio
+        )
+    return transcript
 
-audio_file=open('paht/to/file', 'rb')
-transcript=openai.Audio.transcribe(
-    file = audio_file,
-    model ="whisper-1", 
-    response_format='srt',
-    languaje="jp"
+def translate_srt(file_path):
+    
+    input_data = open(file_path, 'r').read()    #input file path
+    subs=pysrt.from_string(input_data)          #read srt file
+
+    #Define prompt for the translation
+    prompt_base=(
+        "You are going to be a good translator "
+        "I need this text precisely in spanish trying to keep the same meaning "
+        "Translate from [START] to [END]:\n[START]"
     )
 
-'''input_data = open('path/to/file', 'r').read()
-subs=pysrt.from_string(input_data)
+    def translate(text):
+        prompt = prompt_base
+        prompt += text + "\n[END]"
+        
+        response = openai.Completion.create(
+            model='text-davinci-003',
+            prompt=prompt,
+            max_tokens=3000,
+            temperature=0
+        )
+        return response.choices[0].text.strip()
 
-prompt_base=(
-    "You are going to be a good translator "
-    "I need this text precisely in spanish trying to keep the same meaning "
-    "Translate from [START] to [END]:\n[START]"
-)
-
-def translate(text):
-    prompt = prompt_base
-    prompt += text + "\n[END]"
+    for index, subtitule, text in enumerate(subs):
+        subtitule.text = translate(subtitule.text)  
+        print(subtitule)
+    return subtitule
+        
+def save_file(file_path, subs):
+    with open(file_path, 'w') as f:
+        f.write(subs.to_srt())
+        print("File saved successfully!")
+        
+def menu():
+    try:
+        option=int(input('This is an AI powered translator and transcriptor. \nWhich option do you want to use?\n 1. Transcriptor \n 2. Translator \n 3. Exit'))
+    except:
+        menu()
+    if option==1:
+        file=input('Please, enter the path of the audio file: ')
+        transcript=transcript_audio(file)
+        try:    
+            option=int(input('Do you want to save the transcript? \n 1. Yes \n 2. No'))
+        except:
+            print('Please, enter a valid option')
+            option=int(input('Do you want to save the transcript? \n 1. Yes \n 2. No'))
+        if option==1:
+            file=input('Please, enter the path where you want to save the transcript: ')
+            with open(file, 'w') as f:
+                f.write(transcript)
+                print('File saved successfully!')
+        
+        'print(transcript)'
     
-    response = openai.Completion.create(
-        model='text-davinci-003',
-        prompt=prompt,
-        max_tokens=3000,
-        temperature=0
-    )
-    return response.choices[0].text.strip()
+menu()
+'''with open('path/to file', 'w') as file:
+    file.write(transcript)'''
 
-for index, subtitule, text in enumerate(subs):
-    subtitule.text = translate(subtitule.text)  
-    print(subtitule)'''
-    
+'''print(transcript_audio())'''
 
-print(transcript)
 
-with open('path/to file', 'w') as file:
-    file.write(transcript)
 #print(translate)
